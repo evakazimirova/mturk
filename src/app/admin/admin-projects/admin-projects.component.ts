52ae2a1953e2ea567d2aa6346923b8e5883f5c19
iGor Polyakov <igor_polyakov@phystech.edu>
2017-05-16 18:07:46 +0300

    admin projects added

--- src/app/admin/admin-projects/admin-projects.component.ts
+++ src/app/admin/admin-projects/admin-projects.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-projects.component.scss']
 })
 export class AdminProjectsComponent implements OnInit {
+  projects = [
+    {
+      id: 1,
+      title: "Видос_1",
+      status: "Активный",
+      price: 3000
+    },
+    {
+      id: 2,
+      title: "Видос_2",
+      status: "Не активный",
+      price: 2000
+    },
+    {
+      id: 3,
+      title: "Видос_3",
+      status: "Активный",
+      price: 1000
+    }
+  ]
 
   constructor() { }
 
