0e590cb9d24c6c6312df2e575330b4abd981c014
Igor Polyakov <igor_polyakov@phystech.edu>
2017-05-16 23:16:42 +0300

    admin users added

--- src/app/admin/admin-users/admin-users.component.ts
+++ src/app/admin/admin-users/admin-users.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-users.component.scss']
 })
 export class AdminUsersComponent implements OnInit {
+  users = [
+    {
+      firstName: 'Игорь',
+      secondName: 'Поляков',
+      email: 'igor_polyakov@phystech.edu',
+      projects: 2,
+      completed: 5,
+      progress: [
+        10,
+        70
+      ]
+    },
+    {
+      firstName: 'Максим',
+      secondName: 'Рябов',
+      email: 'm.ryabov@neurodatalab.com',
+      projects: 2,
+      completed: 7,
+      progress: [
+        50,
+        83
+      ]
+    }
+  ];
 
   constructor() { }
 
