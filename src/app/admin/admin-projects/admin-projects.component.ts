d3e58e65261cf0fac23e5573cfa8627788ea264c
Igor Polyakov <igor_polyakov@phystech.edu>
2017-05-16 20:56:59 +0300

    admin projects modal fixed

--- src/app/admin/admin-projects/admin-projects.component.ts
+++ src/app/admin/admin-projects/admin-projects.component.ts
@@ -6,26 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-projects.component.scss']
 })
 export class AdminProjectsComponent implements OnInit {
+  projectId = 0;
   projects = [
     {
-      id: 1,
+      id: 0,
       title: "Видос_1",
       status: "Активный",
       price: 3000
     },
     {
-      id: 2,
+      id: 1,
       title: "Видос_2",
       status: "Не активный",
       price: 2000
     },
     {
-      id: 3,
+      id: 2,
       title: "Видос_3",
       status: "Активный",
       price: 1000
     }
-  ]
+  ];
 
   constructor() { }
 
