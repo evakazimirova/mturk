2691bc0b5d3fadef6de1f124e146c0eaf550d07f
Igor Polyakov <igor_polyakov@phystech.edu>
2017-05-19 15:14:21 +0300

    mailer added

--- app/routes/sign.js
+++ app/routes/sign.js
@@ -4,8 +4,37 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const parseJSON = bodyParser.json();
 
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'neurodatalab@gmail.com',
+    pass: 'Mgimofinish2'
+  }
+});
+
+
 typicalPostRequest('/up', function(req) {
   res = req;
+
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"MAX 👻" <info@neurodatalab.com>',
+    to: req.email,
+    subject: 'NeuroDataLab Registration',
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+
   return res;
 });
 
