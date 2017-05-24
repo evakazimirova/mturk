5429e9411bdf3d97c1d7fec0279e9d3f1dbcc7d0
iGor Polyakov <igor_polyakov@phystech.edu>
2017-05-24 15:22:25 +0300

    db tables updated

--- createDB.sql
+++ createDB.sql
@@ -10,10 +10,102 @@ CREATE TABLE Annotators (
   secondName VARCHAR(50) NOT NULL
 )
 
+-- Список заданий выполняемых / выполненных аннотатором. Read/Write
+-- Изменяется при принятии пользователем задания.
+-- В зависимости от типа задания поля TID_M и TID_E принимают соответствующие значения.
+   -- M - из таблицы TaskMarkUP
+   -- E - из таблицы TaskEventSelection
+-- Неиспользуемый ID равный null.
+CREATE TABLE AnnotatorTasks (
+  TID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  AID INT NOT NULL,
+  TID_M INT NOT NULL,
+  TID_E INT NOT NULL,
+  status INT NOT NULL, -- 0 - inactive; 1 - active
+  price float(6) NOT NULL
+)
+
+-- Содержит расшифровку столбцов Ei в таблице TasksMarkUP. Read only
+-- Пример:
+-- E1 EmotionOne
+-- E2 EmotionTwo
+-- ...
+-- E10 EmotionThree
+CREATE TABLE EmotionInfo (
+  shortName VARCHAR(3) NOT NULL PRIMARY KEY,
+  longName VARCHAR(50) NOT NULL,
+  scale INT NOT NULL DEFAULT ((1))
+)
+
+-- Содержит расшифровку записей в столбце EmotionList в таблице TasksEventSelection. Read only
+-- Используется при формировании заголовков колонок в выходном файле с оценками.
+-- Для EventInfo:
+-- Ev1 Речь 1
+-- Ev2 Речь 2
+-- ...
+-- Ev6 реклама
+CREATE TABLE EventInfo (
+  shortName VARCHAR(4) NOT NULL PRIMARY KEY,
+  longName VARCHAR(50) NOT NULL
+)
+
+-- Результаты разметки. Read/write
+-- Под Ei подразумевается множество столбцов с именами E1...EN, типа Integer где N=20.
+CREATE TABLE MarkUPResults (
+  TID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  AID INT NOT NULL,
+  E1 TEXT,
+  E2 TEXT,
+  E3 TEXT,
+  E4 TEXT,
+  E5 TEXT,
+  E6 TEXT,
+  E7 TEXT,
+  E8 TEXT,
+  E9 TEXT,
+  E10 TEXT,
+  E11 TEXT,
+  E12 TEXT,
+  E13 TEXT,
+  E14 TEXT,
+  E15 TEXT,
+  E16 TEXT,
+  E17 TEXT,
+  E18 TEXT,
+  E19 TEXT,
+  E20 TEXT
+)
+
+-- Таблица содержит URI файлов с разметкой и связывает их с соответствующей видеозаписью через VID. Read/write
+-- Поля CID VID индентификаторы разметки и видео соответственно. CID создается при добавлении записи в таблицу. CURI - URI файла с разметкой.
+-- PersonName - имя размечаемого объекта.
+CREATE TABLE PersonSelection (
+  CID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  VID INT NOT NULL,
+  CURI VARCHAR(255) NOT NULL,
+  personName VARCHAR(50) NOT NULL
+)
+
+-- Информация по проектам. Read only
+CREATE TABLE Projects (
+  PID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  projectName VARCHAR(50) NOT NULL,
+  pricePerTask float(6) NOT NULL,
+  annoPerTask	INT NOT NULL
+)
+
 -- Таблица содержит URI необработанных видеозаписей и некоторые характеристики видео. Read Only
 CREATE TABLE RAWVideo (
   VID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
   URI VARCHAR(255) NOT NULL
+)
+
+-- Список заданий нарезки. Read only.
+-- EventList содержит список событий которые надо выделить.
+CREATE TABLE TasksEventSelection (
+  TID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  VID INT NOT NULL,
+  eventList VARCHAR(50) NOT NULL
 )
 
 -- Список заданий разметки. Read only.
@@ -42,96 +134,4 @@ CREATE TABLE TasksMarkUP (
   E18 bit DEFAULT ((0)),
   E19 bit DEFAULT ((0)),
   E20 bit DEFAULT ((0))
-)
-
--- Список заданий нарезки. Read only.
--- EventList содержит список событий которые надо выделить.
-CREATE TABLE TasksEventSelection (
-  TID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  VID INT NOT NULL,
-  eventList VARCHAR(50) NOT NULL
-)
-
--- Таблица содержит URI файлов с разметкой и связывает их с соответствующей видеозаписью через VID. Read/write
--- Поля CID VID индентификаторы разметки и видео соответственно. CID создается при добавлении записи в таблицу. CURI - URI файла с разметкой.
--- PersonName - имя размечаемого объекта.
--- PersonImage - Его изображение.
-CREATE TABLE PersonSelection (
-  TID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  VID INT NOT NULL,
-  CURI VARCHAR(50) NOT NULL,
-  personName VARCHAR(50) NOT NULL,
-  personImage bit DEFAULT ((0))
-)
-
--- Результаты разметки. Read/write
--- Под Ei подразумевается множество столбцов с именами E1...EN, типа Integer где N=20.
-CREATE TABLE MarkUPResults (
-  TID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  AID INT NOT NULL,
-  E1 INT DEFAULT ((-1)),
-  E2 INT DEFAULT ((-1)),
-  E3 INT DEFAULT ((-1)),
-  E4 INT DEFAULT ((-1)),
-  E5 INT DEFAULT ((-1)),
-  E6 INT DEFAULT ((-1)),
-  E7 INT DEFAULT ((-1)),
-  E8 INT DEFAULT ((-1)),
-  E9 INT DEFAULT ((-1)),
-  E10 INT DEFAULT ((-1)),
-  E11 INT DEFAULT ((-1)),
-  E12 INT DEFAULT ((-1)),
-  E13 INT DEFAULT ((-1)),
-  E14 INT DEFAULT ((-1)),
-  E15 INT DEFAULT ((-1)),
-  E16 INT DEFAULT ((-1)),
-  E17 INT DEFAULT ((-1)),
-  E18 INT DEFAULT ((-1)),
-  E19 INT DEFAULT ((-1)),
-  E20 INT DEFAULT ((-1))
-)
-
--- Информация по проектам. Read only
-CREATE TABLE Projects (
-  PID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  projectName VARCHAR(50) NOT NULL,
-  pricePerTask float(6) NOT NULL,
-  annoPerTask	INT NOT NULL
-)
-
--- Список заданий выполняемых / выполненных аннотатором. Read/Write
--- Изменяется при принятии пользователем задания.
--- В зависимости от типа задания поля TID_M и TID_E принимают соответствующие значения.
--- M - из таблицы TaskMarkUP
--- E - из таблицы TaskEventSelection
--- Неиспользуемый ID равный null.
-CREATE TABLE AnnotatorTasks (
-  AID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  TID_M INT NOT NULL,
-  TID_E INT NOT NULL,
-  status INT NOT NULL,
-  price float(6) NOT NULL
-)
-
--- Содержит расшифровку столбцов Ei в таблице TasksMarkUP. Read only
--- Пример:
--- E1 EmotionOne
--- E2 EmotionTwo
--- ...
--- E10 EmotionThree
-CREATE TABLE EmotionInfo (
-  shortName VARCHAR(50) NOT NULL,
-  longName VARCHAR(50) NOT NULL
-)
-
--- Содержит расшифровку записей в столбце EmotionList в таблице TasksEventSelection. Read only
--- Используется при формировании заголовков колонок в выходном файле с оценками.
--- Для EventInfo:
--- Ev1 Речь 1
--- Ev2 Речь 2
--- ...
--- Ev6 реклама
-CREATE TABLE EventInfo (
-  shortName VARCHAR(50) NOT NULL,
-  longName VARCHAR(50) NOT NULL
 )
