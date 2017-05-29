a3fb6c267cf166c25942171a153a73685738ba17
Igor Polyakov <igor_polyakov@phystech.edu>
2017-05-30 01:08:46 +0300

    dont use conf.json

--- src/app/site.component.ts
+++ src/app/site.component.ts
@@ -14,27 +14,20 @@ export class SiteComponent implements OnInit {
   ngOnInit() {
     // читаем файл конфигурации
     // ДОП 2. в конфигурационном файле указывается список шкал для разметки и диапазон разметки. Диапазон может быть либо от 1 до 5 либо от -2 до +2.
-    this.http.get('./assets/conf.json').subscribe(
-      conf => {
-        // применяем конфигурацию
-        this.common.conf = conf;
 
-        // авторизуем пользователя
-        this.http.get('/annotators/authorized').subscribe(
-          user => {
-            if (user) {
-              if (user.chPass) {
-                this.common.mode = "changePassword";
-              } else {
-                this.common.user = user;
-                this.common.mode = "profile";
-              }
-            } else {
-              this.common.mode = "auth";
-            }
-          },
-          error => console.log(error)
-        );
+    // авторизуем пользователя
+    this.http.get('/annotators/authorized').subscribe(
+      user => {
+        if (user) {
+          if (user.chPass) {
+            this.common.mode = "changePassword";
+          } else {
+            this.common.user = user;
+            this.common.mode = "profile";
+          }
+        } else {
+          this.common.mode = "auth";
+        }
       },
       error => console.log(error)
     );
