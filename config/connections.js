a44b0e159566e40db160987f8379e554d9cb6bfa
Igor Polyakov <igor_polyakov@phystech.edu>
2017-06-16 17:38:47 +0300

    port key moved

--- config/connections.js
+++ config/connections.js
@@ -38,8 +38,8 @@ module.exports.connections = {
     password: process.env.DB_PASS,
     host: process.env.DB_SERVER, // azure database
     database: process.env.DB,
-    port: 1433,
     options: {
+      port: 1433,
       encrypt: true // use this for Azure databases
     }
   }
