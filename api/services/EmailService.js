42cb4c75e18547938e0b072aa6279736977b2cae
Igor Polyakov <igor_polyakov@phystech.edu>
2017-07-16 01:47:18 +0300

    common files refac

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const config = {
   service: 'gmail',
   auth: {
-    user: process.env.MAILER_EMAIL,
-    pass: process.env.MAILER_PASS
+    user: sails.config.mail.MAILER_EMAIL || process.env.MAILER_EMAIL,
+    pass: sails.config.mail.MAILER_PASS || process.env.MAILER_PASS
   }
 }
 
