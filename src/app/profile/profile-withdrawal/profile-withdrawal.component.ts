53889082cee1a9b1cc835c103fd50004168f0e29
Igor Polyakov <igor_polyakov@phystech.edu>
2017-07-17 12:02:44 +0300

    c profile-withdrawal refac

--- src/app/profile/profile-withdrawal/profile-withdrawal.component.ts
+++ src/app/profile/profile-withdrawal/profile-withdrawal.component.ts
@@ -7,38 +7,45 @@ import { HttpService } from '../../http.service';
   templateUrl: './profile-withdrawal.component.html',
   styleUrls: ['./profile-withdrawal.component.scss']
 })
-export class ProfileWithdrawalComponent implements OnInit {
+export class ProfileWithdrawalComponent {
+  // данные по умолчанию
   paymentSystem = 'paypal';
   money = 0;
   account = '';
+
+  // индикатор загрузки
   isRequesting = false;
 
-  constructor(public common: CommonService, private http: HttpService) { }
-
-  ngOnInit() {
-  }
+  constructor(public common: CommonService,
+              private http: HttpService) { }
 
   sendRequest() {
     if (!this.isRequesting) {
+      // запрашивае только если хватает денег
       if (this.money <= this.common.user.money.available) {
+        // формируем запрос
         const request = {
           money: this.money,
           system: this.paymentSystem,
           account: this.account
         };
 
+        // отправляем запрос
         this.isRequesting = true;
         this.http.post(request, '/moneyRequests/send').subscribe(
           res => {
-            this.common.user.money.available = res.available;
             this.isRequesting = false;
+
+            // корректируем остаток
+            this.common.user.money.available = res.available;
           },
           err => {
-            console.error(err);
             this.isRequesting = false;
+            console.error(err);
           }
         );
       } else {
+        // сообщаем пользователю, что у него не так много денег для вывода
         alert('You haven\'t earned this amount of money yet. Please, work harder!');
         this.money = this.common.user.money.available;
       }
