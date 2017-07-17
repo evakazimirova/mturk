0c62ee5e0ecae571ccedbb6e3725d7bf73461f9a
Igor Polyakov <igor_polyakov@phystech.edu>
2017-07-17 10:35:47 +0300

    c profile-headed refac

--- src/app/profile/profile-header/profile-header.component.ts
+++ src/app/profile/profile-header/profile-header.component.ts
@@ -1,31 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { HttpService } from '../../http.service';
 import { CommonService } from '../../common.service';
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'na-profile-header',
   templateUrl: './profile-header.component.html',
   styleUrls: ['./profile-header.component.scss']
 })
-export class ProfileHeaderComponent implements OnInit {
+export class ProfileHeaderComponent {
   @Input() profileMode;
   @Output() profileModeSelected = new EventEmitter();
+
+  constructor(public common: CommonService,
+              private http: HttpService) {}
+
   selectProfileMode(page) {
     this.profileModeSelected.emit(page);
   }
 
-  user;
-
-  constructor(public common: CommonService, private http: HttpService) {}
-
-  ngOnInit() {}
-
   signOut() {
-    const confirmed = confirm("Do you really want to sign out?");
-
+    // подтверждение выхода из системы
+    const confirmed = confirm('Do you really want to sign out?');
     if (confirmed) {
       this.http.getRough('/annotators/logout').subscribe(
-        (res) => {
+        res => {
+          // выходим из системы
           this.common.mode = 'auth';
         },
         err => console.log(err)
