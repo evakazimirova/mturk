f3ed4030168e3efc737c31d2047a3521f1a705b4
Igor Polyakov <igor_polyakov@phystech.edu>
2017-07-17 13:35:44 +0300

    c annotating-fragments-ranges refac

--- src/app/annotating/annotating-fragments/annotating-fragments-ranges/annotating-fragments-ranges.component.ts
+++ src/app/annotating/annotating-fragments/annotating-fragments-ranges/annotating-fragments-ranges.component.ts
@@ -1,14 +1,11 @@
 import { CommonService } from '../../../common.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'na-annotating-fragments-ranges',
   templateUrl: './annotating-fragments-ranges.component.html',
   styleUrls: ['./annotating-fragments-ranges.component.scss']
 })
-export class AnnotatingFragmentsRangesComponent implements OnInit {
-
-  constructor(private common: CommonService) {}
-
-  ngOnInit() {}
+export class AnnotatingFragmentsRangesComponent {
+  constructor(public common: CommonService) {}
 }
