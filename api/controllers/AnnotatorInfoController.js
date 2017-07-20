66bff2c64ef82f74a11ed26b1ba3e43beb749211
Igor Polyakov <igor_polyakov@phystech.edu>
2017-07-20 23:41:16 +0300

    AnnotatorInfo api added

---
+++ api/controllers/AnnotatorInfoController.js
@@ -0,0 +1,11 @@
+/**
+ * AnnotatorInfoController
+ *
+ * @description :: Server-side logic for managing Annotatorinfoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
